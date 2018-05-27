ef1373060b963a8ee793c3b8faae3526e94d0060
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2018-05-27 18:11:10 +1000

    Added missing XML documentation comments.

--- src/AppMetricsSerilogReporter/MessageTemplates.cs
+++ src/AppMetricsSerilogReporter/MessageTemplates.cs
@@ -1,23 +1,53 @@
 ﻿namespace AppMetricsSerilogReporter
 {
+    /// <summary>
+    /// Serilog message templates for the different metric types.
+    /// </summary>
     public class MessageTemplates
     {
+        /// <summary>
+        /// Gets or sets the fallback message template to use when the metric types cannot be identified.
+        /// </summary>
         public string Fallback { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message template to use for the Counter metric type.
+        /// </summary>
         public string Counter { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message template to use for Counter item metric type.
+        /// </summary>
         public string CounterItem { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message template to use for the Gauge metric type.
+        /// </summary>
         public string Gauge { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message template to use for the Histogram metric type.
+        /// </summary>
         public string Histogram { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message template to use for the Meter metric type.
+        /// </summary>
         public string Meter { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message template to use for Meter item metric type.
+        /// </summary>
         public string MeterItem { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message template to use for Timer metric type.
+        /// </summary>
         public string Timer { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageTemplates"/> class.
+        /// </summary>
         public MessageTemplates()
         {
             Fallback = "[Metric] Context: {context}, Name: {name}";
